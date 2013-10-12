59131bf16cc2ff4ea50b0087d9f9d42157aeac8c
windyakin <windyakin@gmail.com>
2013-10-12 21:05:06 +0900

    テンプレート解析による表示部分

--- js/NicoMylistEmbedder.js
+++ js/NicoMylistEmbedder.js
@@ -1,35 +1,52 @@
-(function(){
+(function($){
 
-	$(function(){
+	$.fn.NicoMylistEmbedder = function( options ){
+		// 設定の読み込み
+		var defaults = {
+			'mylist': 36922986,
+			'selector': ".nico_mylist",
+			'template': "./js/template.html",
+			'proxy': "./proxy.cgi",
+		};
+		
+		var setting = $.extend(defaults,options);
+		console.log(setting.mylist);
 
-		$.fn.NicoMylistEmbedder = function( options ){
-			// 設定の読み込み
-			var defaults = {
-				'mylist': 36922986,
-				'class': ".nico_mylist"
-			}
-			var setting = $.extend(defaults,options);
-			console.log(setting.mylist);
+		var $template = null;
 
-			// ajaxで取得するよ
-			$.ajax({
-				url: '../proxy.cgi?'+setting.mylist,
-				type: 'get',
-				dataType: 'json',
-			})
-			.done(function(data) {
-				console.dir(data);
-				$(setting.class).html('<a href="'+data.link+'">'+data.title+'</a>');
-			})
-			.fail(function() {
-				console.log("error");
-			})
-			.always(function() {
-				console.log("complete");
-			});
-			
-		}
+		// まずテンプレートファイルを取得
+		$.ajax({
+			url: setting.template,
+			type: 'get',
+			dataType: 'html',
+		})
+		.done(function(data) {
+			$template = data;
+		})
+		.fail(function() {
+			console.log("テンプレートファイルが読み込めませんでした");
+		})
+		.always(function() {
+		});
+		
 
-	});
+		// その次にマイリストをproxy.cgi経由で取得
+		$.ajax({
+			url: setting.proxy+'?'+setting.mylist,
+			type: 'get',
+			dataType: 'json',
+		})
+		.done(function(data) {
+			console.dir(data);
+			// テンプレートに従って表示
+			$.tmpl($template, data).appendTo(setting.selector);
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+		});
+		
+	}
 
-})();
+})(jQuery);
